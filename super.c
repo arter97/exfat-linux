d53392f91cf3617d954ddd105d22c0422368aaa6
Adek Maulana <adekzmaulana@gmail.com>
2018-05-22 03:33:32 +0700

    exfat: push rcu_barrier() from deactivate_locked_super() to filesystems
    * This was introduce in:
      https://github.com/torvalds/linux/commit/8c0a853
    
    Signed-off-by: Adek Maulana <adekzmaulana@gmail.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- super.c
+++ super.c
@@ -3697,6 +3697,11 @@ static int __init exfat_init_inodecache(void)
 
 static void exfat_destroy_inodecache(void)
 {
+	/*
+	 * Make sure all delayed rcu free inodes are flushed before we
+	 * destroy cache.
+	 */
+	rcu_barrier();
 	kmem_cache_destroy(exfat_inode_cachep);
 }
 
