ef64ed8ecf82f48a4bce24061df21237ac2fca8a
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-27 06:04:14 +0900

    exfat: add an empty compat.h
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ compat.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *  Copyright (C) 2012-2013 Samsung Electronics Co., Ltd.
+ */
+
+#ifndef _EXFAT_COMPAT_H
+#define _EXFAT_COMPAT_H
+
+#include <linux/version.h>
+
+#endif /* _EXFAT_COMPAT_H */
--- exfat_fs.h
+++ exfat_fs.h
@@ -11,6 +11,7 @@
 #include <linux/nls.h>
 
 #include "config.h"
+#include "compat.h"
 
 #define EXFAT_SUPER_MAGIC       0x2011BAB0UL
 #define EXFAT_ROOT_INO		1
